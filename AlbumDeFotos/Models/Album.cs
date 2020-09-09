1e968b428da64834f14570dab611783741decb31
Heber Gustavo <heber.gbarbosa@hotmail.com>
2020-09-09 16:53:26 -0300

    Criação das Models e Conexão com o Banco de Dados

---
+++ AlbumDeFotos/Models/Album.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumDeFotos.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(50, ErrorMessage = "Use menos caracteres!")]
+        public string Destino { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string FotoTopo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public DateTime Inicio { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public DateTime Fim { get; set; }
+
+        public ICollection<Imagem> Imagens { get; set; }
+    }
+}
---
+++ AlbumDeFotos/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumDeFotos.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Imagem> Imagens { get; set; }
+    }
+}
